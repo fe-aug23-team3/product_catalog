a75c4f500fafc758ad47559ee9af6aa757f09519
Yaroslav <slavik2607.lviv@gmail.com>
2023-12-09 17:11:05 +0200

    Initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,68 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
-    plugins: ['@typescript-eslint', 'react-hooks', 'prettier'],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true
-        }
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@mate-academy/eslint-config-react-typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react-hooks', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
-        jest: true
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  rules: {
+    // '@typescript-eslint/naming-convention': [
+    //   'warn',
+    //   {
+    //     selector: 'default',
+    //     format: ['camelCase'],
+    //   },
+    //   {
+    //     selector: 'parameter',
+    //     format: ['camelCase'],
+    //     trailingUnderscore: 'allowSingleOrDouble',
+    //   },
+    //   {
+    //     selector: 'class',
+    //     format: ['PascalCase'],
+    //   },
+    //   {
+    //     selector: 'enum',
+    //     format: ['PascalCase', 'UPPER_CASE'],
+    //   },
+    //   {
+    //     selector: 'enumMember',
+    //     format: ['PascalCase', 'UPPER_CASE'],
+    //   },
+    //   {
+    //     selector: 'interface',
+    //     format: ['PascalCase'],
+    //   },
+    //   {
+    //     selector: 'typeAlias',
+    //     format: ['PascalCase'],
+    //   },
+    //   {
+    //     selector: 'typeParameter',
+    //     format: ['PascalCase'],
+    //   },
+    // ],
+    // 'react-hooks/rules-of-hooks': 'warn',
+    // 'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    rules: {
-        '@typescript-eslint/naming-convention': [
-            'warn',
-            {
-                selector: 'default',
-                format: ['camelCase']
-            },
-            {
-                selector: 'parameter',
-                format: ['camelCase'],
-                trailingUnderscore: 'allowSingleOrDouble'
-            },
-            {
-                selector: 'class',
-                format: ['PascalCase']
-            },
-            {
-                selector: 'enum',
-                format: ['PascalCase', 'UPPER_CASE']
-            },
-            {
-                selector: 'enumMember',
-                format: ['PascalCase', 'UPPER_CASE']
-            },
-            {
-                selector: 'interface',
-                format: ['PascalCase']
-            },
-            {
-                selector: 'typeAlias',
-                format: ['PascalCase']
-            },
-            {
-                selector: 'typeParameter',
-                format: ['PascalCase']
-            }
-        ],
-        'react-hooks/rules-of-hooks': 'warn',
-        'react-hooks/exhaustive-deps': 'warn'
-    },
-    settings: {
-        react: {
-            version: 'detect'
-        }
-    }
-}
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import RootComponent from './RootComponent'
-import { persistor, store } from './store/reducers/store'
+import React from 'react';
 
-const App: React.FC = () => {
-    return (
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <RootComponent />
-            </PersistGate>
-        </Provider>
-    )
-}
-
-export default App
+export const App: React.FC = () => {
+  return <h1>Hello World!</h1>;
+};
