6ef0aee6ef3fe8bed999fd2a6705d9913cfaf21d
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-12-11 20:53:34 +0200

    “implemented_category_section”

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './HomePage.module.scss';
+import { CategorySection } from './CategorySection/CategorySection';
 
 export const HomePage: React.FC = () => {
-  return <h1>Hello it is a HomePage!</h1>;
+  return (
+    <>
+      <h1>Hello it is a HomePage!</h1>
+      <CategorySection />
+    </>
+  );
 };
