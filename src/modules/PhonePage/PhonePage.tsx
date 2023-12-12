15eb6d240cf766ef7580723efed7e8508c903023
Yaroslav <slavik2607.lviv@gmail.com>
2023-12-12 15:16:15 +0200

    Add dropdown

--- src/modules/PhonePage/PhonePage.tsx
+++ src/modules/PhonePage/PhonePage.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Dropdown } from './Dropdown';
 
 export const PhonePage: React.FC = () => {
-  return <h1>It is a PhonePage</h1>;
+  return (
+    <section>
+      <h1>Mobile phones</h1>
+      <p>71 models</p>
+      <Dropdown title="Sort by" />
+    </section>
+  );
 };
--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,10 +1,27 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axious from 'axios';
 
-const BASE_URL = 'https://mate.academy/students-api';
+export const BASE_URL = 'https://product-catalog-api-e4vw.onrender.com';
+// const BASE_URL = 'http://localhost:3005';
 
 const requests = {
   get: (pathname: string) => axious.get(`${BASE_URL}${pathname}`),
 };
 
-export const getPhones = () => requests.get('/HERE_will_be_url');
+export const getPhones = (page = 1, amountPhones = 16, sortBy = 'newest') => {
+  return requests.get(
+    `/phones?sortBy=${sortBy}&page=${page}&amountPhones=${amountPhones}`,
+  );
+};
+
+export const getLength = () => requests.get('/phones/length');
+
+export const getOnePhone = (id: any) => requests.get(`/phones:${id}`);
+
+export const getPhonesWithMaxDiscount = () => requests.get('/phones/discount');
+
+export const getTheNewestPhones = () => requests.get('/phones/newest');
+
+export const getRecomendationPhones = (phoneId: string) => {
+  return requests.get(`/phones/:${phoneId}/recomendation`);
+};
