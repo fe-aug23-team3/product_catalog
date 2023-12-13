b87303947904ced6f81986ad563733afae62ef08
Yaroslav <slavik2607.lviv@gmail.com>
2023-12-13 16:31:21 +0200

    Try to fix merge conflict

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getPhones } from '../../utils/fetchClient';
-import { Header } from '../shared/components/header';
+import { Header } from '../shared/components/Header';
 import { CategorySection } from './CategorySection';
 
 export const HomePage: React.FC = () => {
