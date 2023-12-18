b46f788986f22dc2f99785a36590c2d290a587aa
Yarolav <slavik2607.lviv@gmail.com>
2023-12-18 20:47:19 +0200

    Fix-linter-2

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -20,6 +20,8 @@ export const getPhones = (
   );
 };
 
+export const getAllProducts = () => requests.get('/phones/allProducts');
+
 export const getLength = () => requests.get('/phones/length');
 
 export const getOnePhone = (id: number) => requests.get(`/phones:${id}`);
