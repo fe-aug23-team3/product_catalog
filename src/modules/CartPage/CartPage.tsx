579d90b1bb99d1204e168fe671b5cd70bce7eba0
Denys Rosokhach <denzzlle171@gmail.com>
2023-12-18 17:01:47 +0200

    cart PR_version-1

--- src/modules/CartPage/CartPage.tsx
+++ src/modules/CartPage/CartPage.tsx
@@ -8,87 +8,23 @@ import { CartItemType } from './CartTypes/cartItemType';
 import { calculateCartSummary } from './cartUtils';
 import { EmptyCart } from './emptyCart/EmptyCart';
 import { CheckoutModal } from './CheckoutModal/CheckoutModal';
-import { PhonesContext } from '../../store/GlobalProvider'; // new !
-
-// this is temporal fake data
-// const fakeCartData: CartItemType[] = [
-//   {
-//     id: '1',
-//     category: 'phones',
-//     phoneId: 'apple-iphone-7-32gb-black',
-//     itemId: 'apple-iphone-7-32gb-black',
-//     name: 'Apple iPhone 7 32GB Black',
-//     fullPrice: 400,
-//     price: 375,
-//     screen: "4.7' IPS",
-//     capacity: '32GB',
-//     color: 'black',
-//     ram: '2GB',
-//     year: 2016,
-//     image: 'img/phones/apple-iphone-7/black/00.jpg',
-//     quantity: 1,
-//   },
-//   {
-//     id: '2',
-//     category: 'phones',
-//     phoneId: 'apple-iphone-8-32gb-red',
-//     itemId: 'apple-iphone-8-32gb-red',
-//     name: 'Apple iPhone 8 32GB red',
-//     fullPrice: 420,
-//     price: 475,
-//     screen: "4.7' IPS",
-//     capacity: '32GB',
-//     color: 'red',
-//     ram: '2GB',
-//     year: 2017,
-//     image: 'img/phones/apple-iphone-8/black/00.jpg',
-//     quantity: 2,
-//   },
-//   {
-//     id: '3',
-//     category: 'phones',
-//     phoneId: 'apple-iphone-10-64gb-blue',
-//     itemId: 'apple-iphone-10-64gb-blue',
-//     name: 'Apple iPhone 10 64GB Blue',
-//     fullPrice: 550,
-//     price: 500,
-//     screen: "5.1' IPS",
-//     capacity: '64GB',
-//     color: 'blue',
-//     ram: '4GB',
-//     year: 2018,
-//     image: 'img/phones/apple-iphone-10/black/00.jpg',
-//     quantity: 1,
-//   },
-// ];
-//
+import { PhonesContext } from '../../store/GlobalProvider';
 
 export const CartPage: React.FC = () => {
-  // const [cartData, setCartData] = useState<CartItemType[]>(fakeCartData);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
-  const phonesContext = useContext(PhonesContext); // new !
-  const [cartData, setCartData] = useState<CartItemType[]>(phonesContext.cart);// new !
+  const phonesContext = useContext(PhonesContext);
+  const [cartData, setCartData] = useState<CartItemType[]>(phonesContext.cart);
 
   useEffect(() => {
     setCartData(phonesContext.cart);
-  }, [phonesContext.cart]); // new !
+  }, [phonesContext.cart]);
 
-  // const deleteItem = (id: number) => {
-  //   setCartData(cartData.filter((item) => +item.id !== id));
-  // };
-
-  // const changeQuantity = (id: number, newQuantity: number) => {
-  //   setCartData((prevCartData) => prevCartData
-  //     .map((item) => (+item.id === id
-  //       ? { ...item, quantity: newQuantity }
-  //       : item)));
-  // };
   const deleteItem = (id: number) => {
     const newCartData = cartData.filter((item) => +item.id !== id);
 
     setCartData(newCartData);
-    phonesContext.setCart(newCartData); // Обновляем контекст
+    phonesContext.setCart(newCartData);
   };
 
   const changeQuantity = (id: number, newQuantity: number) => {
@@ -98,14 +34,14 @@ export const CartPage: React.FC = () => {
         : item));
 
     setCartData(newCartData);
-    phonesContext.setCart(newCartData); // Обновляем контекст
+    phonesContext.setCart(newCartData);
   };
 
   const cartSummary = calculateCartSummary(cartData);
 
   const handleCheckout = () => {
     setIsModalOpen(true);
-    phonesContext.setCart([]); // Update the context
+    phonesContext.setCart([]);
   };
 
   const handleCloseModal = () => {
