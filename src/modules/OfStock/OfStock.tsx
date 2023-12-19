ef8bd1ef5c838e4fa3292bc1776476e9459120c5
Danylo Morhun <ayogoharo@gmail.com>
2023-12-19 22:48:48 +0200

    desert

--- src/modules/OfStock/OfStock.tsx
+++ src/modules/OfStock/OfStock.tsx
@@ -4,21 +4,21 @@ import { Desert } from './Desert/Desert';
 
 export const OfStock: React.FC = () => {
   return (
-    <main>
-      <div className={style.OfStock}>
-        <div className={style.OfStock__text}>
-          <h2>We are sorry</h2>
+    <div className={style.OfStock}>
+      <div className={style.OfStock__text}>
+        <h2>We are sorry</h2>
 
-          <p>
-            {
-              // eslint-disable-next-line max-len
-              'But this category of goods is temporarily out of stock. Please check back soon for availability. Thank you for your understanding.'
-            }
-          </p>
-        </div>
+        <p>
+          {
+            // eslint-disable-next-line max-len
+            'But this category of goods is temporarily out of stock. Please check back soon for availability. Thank you for your understanding.'
+          }
+        </p>
+      </div>
 
+      <div className={style.OfStock__wrap}>
         <Desert />
       </div>
-    </main>
+    </div>
   );
 };
