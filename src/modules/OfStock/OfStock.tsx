6e87c2c317999979eee48cef234145bcf7a4a130
YevhenKharko <130221952+YevhenKharko@users.noreply.github.com>
2023-12-19 12:59:22 +0200

    overfixes (#35)
    * overfixes
    
    * smooth_pagination

--- src/modules/OfStock/OfStock.tsx
+++ src/modules/OfStock/OfStock.tsx
@@ -4,21 +4,19 @@ import { Desert } from './Desert/Desert';
 
 export const OfStock: React.FC = () => {
   return (
-    <main>
-      <div className={style.OfStock}>
-        <div className={style.OfStock__text}>
-          <h2>We are sorry</h2>
+    <div className={style.OfStock}>
+      <div className={style.OfStock__text}>
+        <h2>We are sorry</h2>
 
-          <p>
-            {
-              // eslint-disable-next-line max-len
-              'But this category of goods is temporarily out of stock. Please check back soon for availability. Thank you for your understanding.'
-            }
-          </p>
-        </div>
-
-        <Desert />
+        <p>
+          {
+            // eslint-disable-next-line max-len
+            'But this category of goods is temporarily out of stock. Please check back soon for availability. Thank you for your understanding.'
+          }
+        </p>
       </div>
-    </main>
+
+      <Desert />
+    </div>
   );
 };
