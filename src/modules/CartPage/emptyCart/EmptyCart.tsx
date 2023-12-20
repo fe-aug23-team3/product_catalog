eb7f67bc664277e4eaadcb4c91023468a5252460
Yarolav <63805271+YaroslavYarynych@users.noreply.github.com>
2023-12-20 16:22:42 +0200

    Revert "Cart new (#41)"
    This reverts commit eadd9258d894043a21a5c83000d30f40971d8170.

--- src/modules/CartPage/emptyCart/EmptyCart.tsx
+++ src/modules/CartPage/emptyCart/EmptyCart.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './EmptyCart.module.scss';
-import CartImage from '../../shared/icons/pngwing1.png';
+import { ReactComponent as CartIcon } from '../../shared/icons/empty_cart.svg';
 
 export const EmptyCart: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <div className={styles.emptyCart}>
-      <img src={CartImage} alt="img" className={styles.icon} />
+      <CartIcon className={styles.icon} />
       <h2 className={styles.emptyCart__title}> Oops! Your Cart is Empty </h2>
       <p className={styles.emptyCart__describe}>
         But you can change that! Explore our product catalog now.
--- src/modules/shared/components/FooterConponent/Footer.tsx
+++ src/modules/shared/components/FooterConponent/Footer.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
 import logo from '../../icons/main-logo.svg';
 import { ReactComponent as ChevronUp } from '../../icons/ChevronUp.svg';
@@ -17,33 +16,22 @@ export const Footer: React.FC = () => {
   return (
     <footer className={styles.container}>
       <div className={styles.footer}>
-        <Link to="/" onClick={() => scrollToTop()}>
-          <img className={styles.logo} src={logo} alt="nice gadgets logo" />
-        </Link>
+        <img className={styles.logo} src={logo} alt="nice gadgets logo" />
 
         <div className={styles.footer_links}>
           <a
             className={styles.footer_link}
-            href="https://github.com/fe-aug23-team3"
-            target="_blank"
-            rel="noreferrer"
+            href="https://github.com/placeholder"
           >
             Github
           </a>
           <a
             className={styles.footer_link}
             href="mailto:placeholder@example.com"
-            target="_blank"
-            rel="noreferrer"
           >
             Contacts
           </a>
-          <a
-            className={styles.footer_link}
-            href="/placeholder"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a className={styles.footer_link} href="/placeholder">
             Rights
           </a>
         </div>
