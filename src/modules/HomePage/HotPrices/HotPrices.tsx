179408d6596880cbbc4795af93971188a5ee874f
YevhenKharko <yevhenkharko@gmail.com>
2023-12-20 10:59:42 +0200

    two loaders completed, need to fix phonepage dropdowns

--- src/modules/HomePage/HotPrices/HotPrices.tsx
+++ src/modules/HomePage/HotPrices/HotPrices.tsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { SliderComponent } from '../../shared/components/SliderComponent';
 import { getPhonesWithMaxDiscount } from '../../../utils/fetchClient';
+import { Loader } from '../../shared/components/loader';
 import { Phone } from '../../../types/Phone';
 
 export const HotPrices: React.FC = () => {
   const [hotModels, setHotModels] = useState<Phone[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    getPhonesWithMaxDiscount().then((res) => setHotModels(res.data));
+    setIsLoading(true);
+    getPhonesWithMaxDiscount()
+      .then((res) => {
+        setHotModels(res.data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
   }, []);
 
-  return <SliderComponent data={hotModels} header="Hot prices" />;
+  return (
+    isLoading ? (
+      <Loader />
+    ) : (
+      <SliderComponent data={hotModels} header="Hot prices" />
+    )
+  );
 };
