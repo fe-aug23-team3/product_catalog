d412b198c839e66f81552d49f49221eabdd96837
YevhenKharko <yevhenkharko@gmail.com>
2023-12-21 11:34:45 +0200

    small fixes

--- src/modules/HomePage/CategorySection/CategorySection.tsx
+++ src/modules/HomePage/CategorySection/CategorySection.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import styles from './CategorySection.module.scss';
 import accessoriesImage from './CategoryPhotos/accessories_photo.png';
 import phoneImage from './CategoryPhotos/phone_photo.png';
 import tabletImage from './CategoryPhotos/tablet_photo.png';
+import { getLength } from '../../../utils/fetchClient';
 
 export const CategorySection: React.FC = () => {
+  const [phonesLength, setPhonesLength] = useState(0);
+
+  useEffect(() => {
+    getLength().then(res => setPhonesLength(res.data));
+  }, []);
+
   return (
     <>
       <section className={styles.category} id="category">
@@ -14,43 +22,45 @@ export const CategorySection: React.FC = () => {
             <article
               className={`${styles.product} ${styles.category__product}`}
             >
-              <a href="/mobile-phones">
+              <NavLink to="/phones">
                 <img
                   src={phoneImage}
                   alt="Mobile phones"
                   className={`${styles.product__photo} ${styles.product__phonePhoto}`}
                 />
                 <h3 className={styles.product__title}>Mobile phones</h3>
-                <p className={styles.product__category}>95 models</p>
-              </a>
+                <p className={styles.product__category}>
+                  {`${phonesLength} models`}
+                </p>
+              </NavLink>
             </article>
 
             <article
               className={`${styles.product} ${styles.category__product}`}
             >
-              <a href="/tablets">
+              <NavLink to="/tablets">
                 <img
                   src={tabletImage}
                   alt="Tablets"
                   className={`${styles.product__photo} ${styles.product__tabletPhoto}`}
                 />
                 <h3 className={styles.product__title}>Tablets</h3>
-                <p className={styles.product__category}>24 models</p>
-              </a>
+                <p className={styles.product__category}>0 models</p>
+              </NavLink>
             </article>
 
             <article
               className={`${styles.product} ${styles.category__product}`}
             >
-              <a href="/accessories">
+              <NavLink to="/accessories">
                 <img
                   src={accessoriesImage}
                   alt="Accessories"
                   className={`${styles.product__photo} ${styles.product__accessoriesPhoto}`}
                 />
                 <h3 className={styles.product__title}>Accessories</h3>
-                <p className={styles.product__category}>100 models</p>
-              </a>
+                <p className={styles.product__category}>0 models</p>
+              </NavLink>
             </article>
           </div>
         </div>
